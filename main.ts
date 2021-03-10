ccaeb52adf8db4a09e8949899d2a71ef98e652f9
Sharon-Daniela-Lopez-Valbuena <80419869+Sharon-Daniela-Lopez-Valbuena@users.noreply.github.com>
2021-03-10 13:28:18 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    while (input.temperature() > 25) {
+        basic.showIcon(IconNames.Happy)
+    }
+    while (input.temperature() < 20) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
